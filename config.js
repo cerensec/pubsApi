a33a41bae12649b747cddb0e96352e72f17be012
cceren <carlosceren2807@gmail.com>
2022-03-14 11:03:47 +0100

    heroku db

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
     db: {
-        host:"",
-        database: "",
-        user: "",
-        password: ""
+        host:"eu-cdbr-west-02.cleardb.net",
+        database: "heroku_cd79302181ae434",
+        user: "b4bb319b627eea",
+        password: "bae02698"
     },
     token:{
         secret:"temporalSecret"
